1ec20504deb4f7e63fcbc43e717365c1fefba827
Denys Dorokhov <hamboomger@gmail.com>
2017-11-06 16:07:04 +0100

    EventsSearchConfiguration: username column added, annotated as @Entity
    New column "username" added.
    Class annotated as @Entity, username field serves as pk(@Id)

--- app/src/main/java/com/hamboomger/model/search/EventsSearchConfiguration.java
+++ app/src/main/java/com/hamboomger/model/search/EventsSearchConfiguration.java
@@ -3,20 +3,42 @@ package com.hamboomger.model.search;
 import com.hamboomger.model.event.EventType;
 import com.hamboomger.model.event.PriceType;
 
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.List;
 
 /**
  * @author ddorochov
  */
+@Entity
 public class EventsSearchConfiguration {
+
+	@Id
+	private String username;
+
+	@ElementCollection
 	private List<String> keywords;
+
+	@ElementCollection
 	private List<EventType> eventTypes;
+
+	@Column
 	private PriceType priceType;
 
-	public EventsSearchConfiguration(List<String> keywords, List<EventType> eventTypes, PriceType priceType) {
+	public EventsSearchConfiguration(String username, List<String> keywords,
+									 List<EventType> eventTypes, PriceType priceType) {
+		this.username = username;
 		this.keywords = keywords;
 		this.eventTypes = eventTypes;
 		this.priceType = priceType;
+	}
+
+	protected EventsSearchConfiguration() {}
+
+	public String getUsername() {
+		return username;
 	}
 
 	public List<String> getKeywords() {
