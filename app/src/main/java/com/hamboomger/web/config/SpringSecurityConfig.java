0eed10847ef4ea3d2b6ac8d2e6113b9f04397055
Denys Dorokhov <hamboomger@gmail.com>
2017-11-25 11:21:39 +0100

    SpringSecurityConfig class and application.properties file added
    - SpringSecurityConfig:
    Configuration class for SpringSecurity.
    
    - application.properties:
    Database configuration (now MySQL instead of H2),
    hibernate, thymeleaf, logging and spring security configuration.

---
+++ app/src/main/java/com/hamboomger/web/config/SpringSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.hamboomger.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+/**
+ * @author ddorochov
+ */
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Value("${spring.queries.users-query}")
+	private String usersQuery;
+
+	@Value("${spring.queries.roles-query}")
+	private String rolesQuery;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+				.usersByUsernameQuery(usersQuery)
+				.authoritiesByUsernameQuery(rolesQuery)
+				.dataSource(dataSource);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/login").anonymous()
+				.antMatchers("/registration").anonymous()
+				.antMatchers("/admin/**").hasAuthority("ADMIN")
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.failureUrl("/login?error=true")
+				.defaultSuccessUrl("/home")
+				.usernameParameter("username")
+				.passwordParameter("password")
+
+				.and().csrf().disable();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+			.ignoring()
+				.antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+	}
+
+}
