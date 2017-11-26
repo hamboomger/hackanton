af0ae74fd35346e6923eb1612209d8ee3113b824
Denys Dorokhov <hamboomger@gmail.com>
2017-11-26 14:25:28 +0100

    SpringTestConfiguration class added

---
+++ app/src/test/java/com/hamboomger/SpringTestConfiguration.java
@@ -0,0 +1,11 @@
+package com.hamboomger;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ddorochov
+ */
+@SpringBootApplication
+public class SpringTestConfiguration {
+
+}
