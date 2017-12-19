e1467c74a9a35d33aa4a8d829a73745c1e2579cb
Denys Dorokhov <hamboomger@gmail.com>
2017-12-19 22:44:33 +0100

    CrosswebMainPageParser now works with url instead of InputStream

--- crossweb/src/main/java/com/hamboomger/crossweb/model/CrosswebMainPageParser.java
+++ crossweb/src/main/java/com/hamboomger/crossweb/model/CrosswebMainPageParser.java
@@ -1,14 +1,35 @@
 package com.hamboomger.crossweb.model;
 
-import java.io.InputStream;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author ddorochov
  */
 public class CrosswebMainPageParser {
-	List<CrosswebEventNameAndLink> getEvents(InputStream is) {
-		return new ArrayList<>();
+	public List<CrosswebEventNameAndLink> getEventsShortInfo(String url) throws IOException {
+		Document doc = Jsoup.connect(url).get();
+		Elements brows = doc.getElementsByClass("brow");
+		if(brows == null)
+			return new ArrayList<>();
+
+		return brows.stream()
+				.map(assembleFunction)
+				.collect(Collectors.toList());
 	}
+
+	private Function<Element, CrosswebEventNameAndLink> assembleFunction = (brow) -> {
+		String pageUrl = "https://crossweb.pl" + brow.attr("href");
+		String title = brow.getElementsByClass("colTab title").text();
+		return new CrosswebEventNameAndLink(title, pageUrl);
+	};
+
 }
--- crossweb/src/test/java/com/hamboomger/crossweb/model/CrosswebMainPageParserTest.java
+++ crossweb/src/test/java/com/hamboomger/crossweb/model/CrosswebMainPageParserTest.java
@@ -1,47 +1,32 @@
 package com.hamboomger.crossweb.model;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author ddorochov
  */
 public class CrosswebMainPageParserTest {
-    List<CrossWebMainArticleTest> articleList = new ArrayList<>();
+    public static final String PAGE_URL = "http://crossweb.pl/en/events/java/";
 
     @Test
     public void testParsing() throws IOException {
         // arrange
         CrosswebMainPageParser parser = new CrosswebMainPageParser();
-        //InputStream is = getClass().getResourceAsStream("/crossweb_main_page.html");
-        Document doc = Jsoup.connect("http://crossweb.pl/en/events/java/").get();
-
-        Elements brows = doc.getElementsByClass("brow");
-        for (Element brow : brows) {
-            String url = "https://crossweb.pl/" + brow.attr("href");
-            String title = brow.getElementsByClass("colTab title").text();
-            articleList.add(new CrossWebMainArticleTest(url, title));
-        }
-
-        articleList.forEach(System.out::println);
-
-        //System.out.println("------------\n"+articleList.get(0)); // для себя проверял )
-        //assertTrue(is != null);
-        //assertTrue(is.available() > 0);
 
         // act
-        //List<CrosswebEventNameAndLink> events = parser.getEvents(is);
+        List<CrosswebEventNameAndLink> events = parser.getEventsShortInfo(PAGE_URL);
 
         // assert
-        //assertTrue(events != null);
-//		assertTrue(!events.isEmpty());
+        assertTrue(events != null);
+		assertTrue(!events.isEmpty());
+		events.forEach(event -> System.out.println(String.format(
+				"Event name: %s\nEvent link:%s \n", event.getName(), event.getUrl()
+		)));
     }
 
 }
